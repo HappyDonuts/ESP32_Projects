5390f680dcae931bb4bac746a06ce66c4f957a74
Javier Morales <javimorales8@gmail.com>
2022-08-07 11:45:13 +0200

    I2C handler library WIP

--- ESP32_OLED_I2C/main/main.c
+++ ESP32_OLED_I2C/main/main.c
@@ -3,38 +3,14 @@
 #include <freertos/task.h>
 #include <esp_log.h>
 
-#include "driver/i2c.h"
-#include "ssd1306_basic.h"
+#include "ESP32_I2C_Handler/esp32_i2c_handler.h"
+#include "SSD1306/ssd1306_basic.h"
 
 static const char *TAG = "i2c-simple-example";
 
-#define I2C_MASTER_SCL_IO           22      /*!< GPIO number used for I2C master clock */
-#define I2C_MASTER_SDA_IO           21      /*!< GPIO number used for I2C master data  */
-#define I2C_MASTER_NUM              0                          /*!< I2C master i2c port number, the number of i2c peripheral interfaces available will depend on the chip */
-#define I2C_MASTER_FREQ_HZ          100000                     /*!< I2C master clock frequency */
-#define I2C_MASTER_TX_BUF_DISABLE   0                          /*!< I2C master doesn't need buffer */
-#define I2C_MASTER_RX_BUF_DISABLE   0                          /*!< I2C master doesn't need buffer */
-#define I2C_MASTER_TIMEOUT_MS       1000
 
-#define ADDRESS 0x3C
 
-esp_err_t i2c_master_init(void)
-{
-    int i2c_master_port = I2C_MASTER_NUM;
 
-    i2c_config_t conf = {
-        .mode = I2C_MODE_MASTER,
-        .sda_io_num = I2C_MASTER_SDA_IO,
-        .scl_io_num = I2C_MASTER_SCL_IO,
-        .sda_pullup_en = GPIO_PULLUP_ENABLE,
-        .scl_pullup_en = GPIO_PULLUP_ENABLE,
-        .master.clk_speed = I2C_MASTER_FREQ_HZ,
-    };
-
-    i2c_param_config(i2c_master_port, &conf);
-
-    return i2c_driver_install(i2c_master_port, conf.mode, I2C_MASTER_RX_BUF_DISABLE, I2C_MASTER_TX_BUF_DISABLE, 0);
-}
 
 void app_main(void)
 {
